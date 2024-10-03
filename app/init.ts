7f153631d2d6b122b7c780a910c6f34f688c4803
yjgaia <yjdev88@gmail.com>
2024-10-03 13:57:38 +0900

    Add new views for game, material, and publisher info

--- app/init.ts
+++ app/init.ts
@@ -1,12 +1,14 @@
 import { Router, SPAInitializer } from "@common-module/app";
 import { AppCompConfig } from "@common-module/app-components";
 import { MaterialLoadingSpinner } from "@common-module/material-loading-spinner";
+import { UniversalWalletConnector } from "@common-module/wallet";
 import { WalletLoginManager } from "@common-module/wallet-login";
 import { SupabaseConnector } from "../../supabase-module/lib/index.js";
 import AppConfig, { IAppConfig } from "./AppConfig.js";
 import LoginView from "./auth/LoginView.js";
 import LogoutView from "./auth/LogoutView.js";
 import NewGameView from "./game/NewGameView.js";
+import NewMaterialView from "./material/NewMaterialView.js";
 
 export default async function init(config: IAppConfig) {
   AppConfig.init(config);
@@ -19,7 +21,24 @@ export default async function init(config: IAppConfig) {
     WalletLoginManager,
   );
 
-  Router.add("/game/new", NewGameView);
+  UniversalWalletConnector.init({
+    name: "material.tech",
+    icon: "https://material.tech/images/icon-192x192.png",
+    description: "ItemFi for web3 games",
+    walletConnectProjectId: "f526c5c4fbfa798d5337194a23b982ed",
+    chains: {
+      "base-sepolia": {
+        id: 84532,
+        name: "Base Sepolia Testnet",
+        symbol: "ETH",
+        rpc: "https://sepolia.base.org",
+        explorerUrl: "https://sepolia.basescan.org",
+      },
+    },
+  });
+
   Router.add("/login", LoginView);
   Router.add("/logout", LogoutView);
+  Router.add("/material/new", NewMaterialView);
+  Router.add("/game/new", NewGameView);
 }
--- app/material/NewMaterialView.ts
+++ app/material/NewMaterialView.ts
@@ -1,8 +1,36 @@
-import { BodyNode, el, View } from "@common-module/app";
+import { BodyNode, el, Router, View } from "@common-module/app";
+import { Button, ButtonType, Form, Input } from "@common-module/app-components";
+import { WalletLoginManager } from "@common-module/wallet-login";
 
 export default class NewMaterialView extends View {
   constructor() {
     super();
-    this.container = el(".new-material-view").appendTo(BodyNode);
+    this.container = el(
+      ".new-material-view",
+      el("header", el("h1", "Create New Material")),
+      new Form(
+        new Input({
+          label: "Token Name",
+          placeholder: "e.g., MyToken",
+          required: true,
+        }),
+        new Input({
+          label: "Token Symbol",
+          placeholder: "e.g., MTK",
+          required: true,
+        }),
+      ),
+      el(
+        "footer",
+        new Button({
+          type: ButtonType.Contained,
+          title: "Create",
+        }),
+      ),
+    ).appendTo(BodyNode);
+
+    if (!WalletLoginManager.isLoggedIn) {
+      Router.goWithoutHistory("/login", { redirectTo: "/material/new" });
+    }
   }
 }
