fd9f1216f583c00e1aa46eed98f05a4e8404b210
yjgaia <yjdev88@gmail.com>
2024-10-04 14:51:52 +0900

    Update webpack output filenames for development and testnet environments

--- supabase-testnet/supabase/functions/api/index.ts
+++ supabase-testnet/supabase/functions/api/index.ts
@@ -1,6 +1,7 @@
 import { startServer } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
 import { serveWalletApi } from "https://raw.githubusercontent.com/yjgaia/wallet-login-module/main/deno/wallet.ts";
 import { serveContractApi } from "https://raw.githubusercontent.com/yjgaia/contract-module/refs/heads/main/deno/contract.ts";
+import MaterialTradeContract from "https://raw.githubusercontent.com/gaiaprotocol/gaiaprotocol-module/refs/heads/main/deno/contracts/MaterialTradeContract.ts";
 
 startServer(
   serveWalletApi,
@@ -8,7 +9,16 @@ startServer(
     {
       "base-sepolia": "https://sepolia.base.org",
     },
-    {},
-    {},
+    {
+      MaterialTrade: MaterialTradeContract,
+    },
+    {
+      MaterialTrade: {
+        "base-sepolia": "0x3Fcea379bCc53Df5062Bc9428FEFb5c495bE75be",
+      },
+    },
+    {
+      MaterialTrade: 15865079,
+    },
   ),
 );
