f5ab802fcfeb63c6a3db9f4bf003f5c95e9bf996
yjgaia <yjdev88@gmail.com>
2024-11-30 13:51:32 +0900

    Add new game input components and update styles for avatar input

---
+++ app/console/game/GameScreenshotInput.ts
@@ -0,0 +1,7 @@
+import { DomNode } from "@common-module/app";
+
+export default class GameScreenshotInput extends DomNode {
+  constructor() {
+    super(".game-screenshot-input");
+  }
+}
---
+++ app/console/game/GameThumbnailInput.ts
@@ -0,0 +1,7 @@
+import { DomNode } from "@common-module/app";
+
+export default class GameThumbnailInput extends DomNode {
+  constructor() {
+    super(".game-thumbnail-input");
+  }
+}
