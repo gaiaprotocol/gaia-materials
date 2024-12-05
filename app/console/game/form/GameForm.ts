c88c7bac944a295119e326c967576b9c8acc1f7e
yjgaia <yjdev88@gmail.com>
2024-12-05 21:04:16 +0900

    Enhance GameForm component with additional input fields; add summary, description, and trailer inputs, and improve layout for thumbnail and screenshot inputs

--- app/console/game/form/GameForm.ts
+++ app/console/game/form/GameForm.ts
@@ -1,13 +1,88 @@
-import { DomNode } from "@common-module/app";
+import { DomNode, el } from "@common-module/app";
+import { Input } from "@common-module/app-components";
+import { GameEntity } from "gaiaprotocol";
 import GameScreenshotInput from "./GameScreenshotInput.js";
 import GameThumbnailInput from "./GameThumbnailInput.js";
 
-export default class GameForm extends DomNode {
-  constructor() {
+export default class GameForm extends DomNode<HTMLDivElement, {
+  dataChanged: (data: GameEntity) => void;
+}> {
+  private data: GameEntity;
+
+  constructor(data?: GameEntity) {
     super(".game-form");
+
+    if (data) this.data = data;
+    else {
+      this.data = {
+        id: -1,
+        name: "",
+        slug: "",
+        is_public: false,
+        screenshots: [],
+        created_at: new Date().toISOString(),
+      };
+    }
+
     this.append(
-      new GameThumbnailInput(),
-      new GameScreenshotInput([]),
+      el(
+        ".name-input-container",
+        new Input({
+          label: "Name",
+          placeholder: "Enter game name",
+          value: this.data.name,
+          onChange: (newValue) => {
+            this.data.name = newValue;
+            this.emit("dataChanged", this.data);
+          },
+        }),
+      ),
+      el(
+        ".summary-input-container",
+        new Input({
+          label: "Summary",
+          placeholder: "Enter game summary",
+          value: this.data.summary,
+          onChange: (newValue) => {
+            this.data.summary = newValue;
+            this.emit("dataChanged", this.data);
+          },
+        }),
+      ),
+      el(
+        ".description-input-container",
+        new Input({
+          label: "Description",
+          placeholder: "Enter game description",
+          value: this.data.description,
+          onChange: (newValue) => {
+            this.data.description = newValue;
+            this.emit("dataChanged", this.data);
+          },
+        }),
+      ),
+      el(
+        ".thumbnail-input-container",
+        el("label", "Thumbnail"),
+        new GameThumbnailInput(this.data.thumbnail_url),
+      ),
+      el(
+        ".screenshot-input-container",
+        el("label", "Screenshots"),
+        new GameScreenshotInput(this.data.screenshots),
+      ),
+      el(
+        ".trailer-input-container",
+        new Input({
+          label: "Trailer",
+          placeholder: "Enter game trailer URL",
+          value: this.data.trailer_url,
+          onChange: (newValue) => {
+            this.data.trailer_url = newValue;
+            this.emit("dataChanged", this.data);
+          },
+        }),
+      ),
     );
   }
 }
