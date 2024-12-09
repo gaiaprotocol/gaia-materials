e0549c61797c737b0a88d8c1d685e05d042d5c30
yjgaia <yjdev88@gmail.com>
2024-12-09 20:48:33 +0900

    Enhance EditMaterialView and MaterialForm: implement editing functionality for material name and symbol using prompt dialogs; update styles for input containers; refactor form options to support edit actions

--- app/console/material/EditMaterialView.ts
+++ app/console/material/EditMaterialView.ts
@@ -1,10 +1,21 @@
 import { el, View } from "@common-module/app";
-import { Button, ButtonType, ErrorDialog } from "@common-module/app-components";
-import { MaterialDataManager, MaterialEntity } from "gaiaprotocol";
+import {
+  Button,
+  ButtonType,
+  ErrorDialog,
+  PromptDialog,
+} from "@common-module/app-components";
+import {
+  GaiaProtocolConfig,
+  MaterialContract,
+  MaterialDataManager,
+  MaterialEntity,
+} from "gaiaprotocol";
 import ConsoleLayout from "../ConsoleLayout.js";
 import MaterialForm from "./form/MaterialForm.js";
 
 export default class EditMaterialView extends View {
+  private address: `0x${string}` | undefined;
   private form: MaterialForm;
 
   constructor() {
@@ -12,7 +23,37 @@ export default class EditMaterialView extends View {
     ConsoleLayout.content = this.container = el(
       ".new-material-view",
       el("header", el("h2", "Edit material")),
-      el("main", this.form = new MaterialForm({ editMode: true })),
+      el(
+        "main",
+        this.form = new MaterialForm({
+          editName: async () => {
+            const newName = await new PromptDialog({
+              title: "Edit Material Name",
+              message: "Enter the new name for the material",
+              value: this.form.data?.name,
+            }).waitForConfirmation();
+
+            if (this.address && this.form.data) {
+              await new MaterialContract(this.address).setName(newName);
+              await this.syncMaterialMetadataToDB();
+              this.form.data = { ...this.form.data, name: newName };
+            }
+          },
+          editSymbol: async () => {
+            const newSymbol = await new PromptDialog({
+              title: "Edit Material Symbol",
+              message: "Enter the new symbol for the material",
+              value: this.form.data?.symbol,
+            }).waitForConfirmation();
+
+            if (this.address && this.form.data) {
+              await new MaterialContract(this.address).setSymbol(newSymbol);
+              await this.syncMaterialMetadataToDB();
+              this.form.data = { ...this.form.data, symbol: newSymbol };
+            }
+          },
+        }),
+      ),
       el(
         "footer",
         new Button({
@@ -34,18 +75,30 @@ export default class EditMaterialView extends View {
     );
   }
 
-  public async changeData(data: { address: string }) {
+  public async changeData(data: { address: `0x${string}` }) {
+    this.address = data.address;
     let material: MaterialEntity | undefined = data as MaterialEntity;
     if (!("game_id" in material) && data.address) {
       material = await MaterialDataManager.getMaterial(data.address);
+      this.address = material?.address;
     }
 
     this.form.data = material;
   }
 
+  private async syncMaterialMetadataToDB(): Promise<void> {
+    if (!this.address) throw new Error("Material address is required");
+
+    await GaiaProtocolConfig.supabaseConnector.callEdgeFunction(
+      "sync-material-metadata-to-db",
+      { address: this.address },
+    );
+  }
+
   private async saveMaterial(): Promise<void> {
     if (!this.form.data) throw new Error("Material data is required");
 
-    //TODO: Implement saveMaterial
+    await MaterialDataManager.updateMaterial(this.form.data);
+    await this.syncMaterialMetadataToDB();
   }
 }
--- app/console/material/form/MaterialForm.ts
+++ app/console/material/form/MaterialForm.ts
@@ -4,7 +4,8 @@ import { LogoInput, MaterialEntity } from "gaiaprotocol";
 import { LogoData } from "gaiaprotocol/lib/form/LogoInput.js";
 
 interface MaterialFormOptions {
-  editMode?: boolean;
+  editName?: () => Promise<void>;
+  editSymbol?: () => Promise<void>;
 }
 
 export default class MaterialForm extends DomNode<HTMLDivElement, {
@@ -31,61 +32,60 @@ export default class MaterialForm extends DomNode<HTMLDivElement, {
           this.emit("dataChanged", this._data);
         },
       }),
-      ...options.editMode
-        ? [
+      options.editName
+        ? el(
+          ".name-input-container",
+          el("label", "Name"),
           el(
-            ".name-input-container",
-            el("label", "Name"),
-            el(
-              ".input-container",
-              this.inputs.name = new Input({ readOnly: true }),
-              new Button(".edit", {
-                type: ButtonType.Contained,
-                title: "Edit",
-                onClick: () => {
-                },
-              }),
-            ),
-          ),
-          el(
-            ".symbol-input-container",
-            el("label", "Symbol"),
-            el(
-              ".input-container",
-              this.inputs.symbol = new Input({ readOnly: true }),
-              new Button(".edit", {
-                type: ButtonType.Contained,
-                title: "Edit",
-                onClick: () => {
-                },
-              }),
-            ),
-          ),
-        ]
-        : [
-          el(
-            ".name-input-container",
-            this.inputs.name = new Input({
-              label: "Name",
-              placeholder: "Enter material name",
-              onChange: (newValue) => {
-                this._data.name = newValue;
-                this.emit("dataChanged", this._data);
+            ".input-container",
+            this.inputs.name = new Input({ readOnly: true }),
+            new Button(".edit", {
+              type: ButtonType.Contained,
+              title: "Edit",
+              onClick: async () => {
+                if (this._data.address) await options.editName!();
               },
             }),
           ),
+        )
+        : el(
+          ".name-input-container",
+          this.inputs.name = new Input({
+            label: "Name",
+            placeholder: "Enter material name",
+            onChange: (newValue) => {
+              this._data.name = newValue;
+              this.emit("dataChanged", this._data);
+            },
+          }),
+        ),
+      options.editSymbol
+        ? el(
+          ".symbol-input-container",
+          el("label", "Symbol"),
           el(
-            ".symbol-input-container",
-            this.inputs.symbol = new Input({
-              label: "Symbol",
-              placeholder: "Enter material symbol",
-              onChange: (newValue) => {
-                this._data.symbol = newValue;
-                this.emit("dataChanged", this._data);
+            ".input-container",
+            this.inputs.symbol = new Input({ readOnly: true }),
+            new Button(".edit", {
+              type: ButtonType.Contained,
+              title: "Edit",
+              onClick: async () => {
+                if (this._data.address) await options.editSymbol!();
               },
             }),
           ),
-        ],
+        )
+        : el(
+          ".symbol-input-container",
+          this.inputs.symbol = new Input({
+            label: "Symbol",
+            placeholder: "Enter material symbol",
+            onChange: (newValue) => {
+              this._data.symbol = newValue;
+              this.emit("dataChanged", this._data);
+            },
+          }),
+        ),
       el(
         ".description-input-container",
         this.inputs.description = new Input({
