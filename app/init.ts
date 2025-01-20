53f7d7f4dd2ab04aba3c17c405ecf6c0fc976d38
yjgaia <yjdev88@gmail.com>
2025-01-20 14:14:03 +0900

    Remove WalletLoginManager initialization from app init function to streamline startup process

--- app/init.ts
+++ app/init.ts
@@ -1,5 +1,4 @@
 import { Router, SPAInitializer } from "@common-module/app";
-import { WalletLoginManager } from "@common-module/wallet-login";
 import AppConfig, { IAppConfig } from "./AppConfig.js";
 import ConsoleLayout from "./console/ConsoleLayout.js";
 import ConsoleView from "./console/ConsoleView.js";
@@ -17,7 +16,6 @@ import Layout from "./views/Layout.js";
 export default async function init(config: IAppConfig) {
   AppConfig.init(config);
   SPAInitializer.init();
-  WalletLoginManager.init();
 
   Router
     .add("/*", Layout, ["/console", "/console/*"])
